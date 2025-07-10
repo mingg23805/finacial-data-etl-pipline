2dc9a15045702decb4582a79b271d790456a1fe5
mingg23805 <nnminh23805@gmail.com>
2025-07-10 17:23:50 +0700

    change file structure

--- backend/script/load/load_parquet_to_database_1.py
+++ backend/script/load/load_parquet_to_database_1.py
@@ -1,10 +1,15 @@
-
 from sqlalchemy import create_engine
-from configfile.io_utils import read_parquet
-from configfile import logger_utils
+import pandas as pd
+
+from configfile.io_utils import read_parquet_file
+from configfile.logger_utils import setup_logger
 logger=setup_logger(__name__)
 def load_company_data_to_database(engine, file_path):
     """
     Load company data to the database.
     """
-    print("Company data loaded to database.")
+    file_path = "backend/data/processed/" + file_path
+    logger.info(f"Loading company data from {file_path} to database.")
+    df = read_parquet_file(file_path)
+    df.to_sql("company_data", con=engine, if_exists="replace", index=False)
+    logger.info("Company data loaded to database.")
